f4bddedf41c651963259fb083ec142b41045de63
Макаров Сергей <makar1031@yandex.ru>
2016-12-17 19:02:14 +0300

    переупорядочивание свойств

--- src/main/java/ru/makar/cource/project/ExperimentLauncher.java
+++ src/main/java/ru/makar/cource/project/ExperimentLauncher.java
@@ -3,7 +3,10 @@ package ru.makar.cource.project;
 import ec.Evolve;
 
 public class ExperimentLauncher {
+
+    private static final String PROPERTY_FILE = "src/main/resources/params.properties";
+
     public static void launch() {
-        Evolve.main(new String[]{Evolve.A_FILE, "src/main/resources/params.properties"});
+        Evolve.main(new String[]{Evolve.A_FILE, PROPERTY_FILE});
     }
 }
