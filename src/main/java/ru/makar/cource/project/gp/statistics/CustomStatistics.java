ffa90068ca003d316ca7183c329b44aeff2c6e1c
Макаров Сергей <makar1031@yandex.ru>
2017-01-06 23:39:18 +0300

    обновление сбора статистики

--- src/main/java/ru/makar/cource/project/gp/statistics/CustomStatistics.java
+++ src/main/java/ru/makar/cource/project/gp/statistics/CustomStatistics.java
@@ -4,6 +4,7 @@ import ec.EvolutionState;
 import ec.gp.koza.KozaFitness;
 import ec.simple.SimpleStatistics;
 import ec.util.Parameter;
+import org.apache.commons.lang3.tuple.MutablePair;
 import ru.makar.cource.project.util.FieldDataStore;
 
 import java.io.IOException;
@@ -11,23 +12,25 @@ import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
 import static java.nio.channels.FileChannel.MapMode.READ_WRITE;
 import static java.nio.file.StandardOpenOption.*;
 
 public final class CustomStatistics extends SimpleStatistics {
-    private static final Parameter GENERATIONS_PARAMETER = new Parameter("generations");
     private static final Parameter JOBS_PARAMETER = new Parameter("jobs");
     private static final Parameter DEFAULT_PARAMETER = null;
+    private static final String FILE_NAME = "statistics.txt";
 
-    private static int[] statistics;
+    private static Map<Integer, MutablePair<Integer, Integer>> statistics;
     private static int maxFood;
 
     @Override
     public void setup(EvolutionState state, Parameter base) {
         super.setup(state, base);
         if (statistics == null) {
-            statistics = new int[state.parameters.getInt(GENERATIONS_PARAMETER, DEFAULT_PARAMETER)];
+            statistics = new HashMap<>();
             maxFood = FieldDataStore.getCurrentInstance().getData().getMaxFood();
         }
     }
@@ -35,24 +38,41 @@ public final class CustomStatistics extends SimpleStatistics {
     @Override
     public void finalStatistics(EvolutionState state, int result) {
         super.finalStatistics(state, result);
-        if (((KozaFitness) best_of_run[0].fitness).hits == maxFood) {
-            statistics[state.generation]++;
+        KozaFitness fitness = (KozaFitness) best_of_run[0].fitness;
+        int hits = fitness.hits;
+        boolean isCollectAllFood = hits == maxFood;
+        if (statistics.containsKey(state.generation)) {
+            MutablePair<Integer, Integer> pair = statistics.get(state.generation);
+            if (hits > pair.getLeft()) {
+                pair.setLeft(hits);
+            }
+            if (isCollectAllFood) {
+                pair.setRight(pair.getRight() + 1);
+            }
+        } else {
+            statistics.put(state.generation, new MutablePair<>(hits, isCollectAllFood ? 1 : 0));
         }
+
         int currentJob = (int) state.job[0] + 1;
         if (currentJob == state.parameters.getInt(JOBS_PARAMETER, DEFAULT_PARAMETER)) {
-            StringBuilder sb = new StringBuilder();
-            for (int generation = 0; generation < statistics.length; generation++) {
-                sb.append(generation).append(" ").append(statistics[generation]).append("\n");
-            }
-            byte[] bytes = sb.toString().getBytes();
-            try {
-                FileChannel channel = (FileChannel) Files
-                        .newByteChannel(Paths.get("statistics.txt"), READ, WRITE, CREATE);
-                MappedByteBuffer buffer = channel.map(READ_WRITE, 0, bytes.length);
-                buffer.put(bytes);
-            } catch (IOException e) {
-                System.out.println("Произошла ошибка при записи в файл: " + e.getLocalizedMessage());
-            }
+            writeStatisticsToFile();
+        }
+    }
+
+    private void writeStatisticsToFile() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("GEN | MAX_FOOD | FIND_OPTIMAL").append("\n");
+        for (Integer key : statistics.keySet()) {
+            MutablePair<Integer, Integer> pair = statistics.get(key);
+            sb.append(key).append(" ").append(pair.getLeft()).append(" ").append(pair.getRight()).append("\n");
+        }
+        byte[] bytes = sb.toString().getBytes();
+        try {
+            FileChannel channel = (FileChannel) Files.newByteChannel(Paths.get(FILE_NAME), READ, WRITE, CREATE);
+            MappedByteBuffer buffer = channel.map(READ_WRITE, 0, bytes.length);
+            buffer.put(bytes);
+        } catch (IOException e) {
+            System.out.println("Произошла ошибка при записи в файл: " + e.getLocalizedMessage());
         }
     }
 }
